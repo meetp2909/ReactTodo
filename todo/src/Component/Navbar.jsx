cef18a3740c95ca96968e1b1fc3cd56d194034a6
meetp2909 <95129051+meetp2909@users.noreply.github.com>
2024-03-09 04:22:00 +0530

    first commit

---
+++ todo/src/Component/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () =>{
+    return(
+        <>
+        
+        <nav className="flex bg-slate-950 text-white list-none gap-20">
+            <li>Home</li>
+            <li>About</li>
+        </nav>
+        </>
+    )
+}
+
+export default Navbar
